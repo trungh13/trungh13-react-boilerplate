7e3a37387e452b255efdf083dccabb647f076a04
trungh13 <newbie.trungh13@gmail.com>
2018-07-04 11:00:02 +0300

    modify eslint

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './App.css';
 
-export default () => (
+const App = () => (
   <div className={styles.App}>
     <h1>Hello to trungh13-react-boilerplate</h1>
   </div>
 );
+export default App;
